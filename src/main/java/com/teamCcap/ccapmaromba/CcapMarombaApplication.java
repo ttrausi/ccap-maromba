aacbe0f5c36ea98a26f0e0081b34fee00351e014
Tainara <tainara.trausi@mercadolivre.com>
2022-08-01 19:10:29 -0300

    first commit

---
+++ src/main/java/com/teamCcap/ccapmaromba/CcapMarombaApplication.java
@@ -0,0 +1,13 @@
+package com.teamCcap.ccapmaromba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcapMarombaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcapMarombaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamCcap/ccapmaromba/CcapMarombaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamCcap.ccapmaromba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcapMarombaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
